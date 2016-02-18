3da4282fbf8407e0ea17bcf6c8291be69bd000a1
David Keegan <git@davidkeegan.com>
2016-02-18 00:30:36 -0800

    making everything public

--- Source/JSONMagic.swift
+++ Source/JSONMagic.swift
@@ -6,23 +6,23 @@
 //  Copyright © David Keegan. All rights reserved.
 //
 
-class JSONMagic {
+public class JSONMagic {
 
     /// The value of the item
-    let value: AnyObject?
+    public let value: AnyObject?
 
     /// Create an instance with any object
-    init(_ value: AnyObject? = nil) {
+    public init(_ value: AnyObject? = nil) {
         self.value = value
     }
 
     /// Create an instance with json data
-    convenience init(data: NSData) {
+    public convenience init(data: NSData) {
         self.init(try? NSJSONSerialization.JSONObjectWithData(data, options: []))
     }
 
     /// Get the item with the key of the dictionary
-    func get(key: String) -> JSONMagic {
+    public func get(key: String) -> JSONMagic {
         if let subData = self.value as? NSDictionary {
             return JSONMagic(subData[key])
         }
@@ -30,7 +30,7 @@ class JSONMagic {
     }
 
     /// Get the item at the index of the array
-    func get(index: Int) -> JSONMagic {
+    public func get(index: Int) -> JSONMagic {
         if let subData = self.value as? NSArray where index < subData.count {
             return JSONMagic(subData[index])
         }
@@ -38,7 +38,7 @@ class JSONMagic {
     }
 
     /// Get the first item of the array
-    var first: JSONMagic {
+    public var first: JSONMagic {
         if let subData = self.value as? NSArray {
             return JSONMagic(subData[0])
         }
@@ -46,7 +46,7 @@ class JSONMagic {
     }
 
     /// Get the last item of the array
-    var last: JSONMagic {
+    public var last: JSONMagic {
         if let subData = self.value as? NSArray {
             return JSONMagic(subData[subData.count-1])
         }
